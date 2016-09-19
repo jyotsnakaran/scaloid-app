56da03b571d58bd3f51f0825d4befcf3180c10ab
SangeetaGulia <sangeeta.gulia@knoldus.in>
2016-09-19 18:47:42 +0530

    Added implementation for MagneticCardRead

--- src/main/scala/scaloid/example/HelloScaloid.scala
+++ src/main/scala/scaloid/example/HelloScaloid.scala
@@ -11,32 +11,24 @@ class HelloScaloid extends SActivity with SContext {
 
     contentView = new SVerticalLayout{
 
-
-      val uniqueId = getUniqueId
-
-  /*    val view = new SWebView()
-      view.setVerticalScrollBarEnabled(true)
-      view.setBackgroundColor(Color.WHITE)
-      view.here*/
-
-      val currentView: View = findViewById(uniqueId)
-      currentView.setBackground(Color.WHITE)
-    /*  val rootView1 = currentView.getRootView
-
-      rootView1.setBackgroundColor(Color.WHITE)
-      */
       style {
         case b: SButton => b.textColor(Color.RED)
         case t: STextView => t textSize 10.dip
         case e: SEditText => e.backgroundColor(Color.YELLOW).textColor(Color.BLACK)
       }
 
-      lazy val swipeMagneticCard = new SButton("Heyyy Click Me").onClick{
+      lazy val swipeMagneticCard = new SButton("Magnetic Card").onClick{
+        val intent = SIntent[MagneticCardActivity]
+        startActivity(intent)
+      }
+
+      lazy val insertChip = new SButton("IC Card").onClick{
         val intent = SIntent[TestActivity]
         startActivity(intent)
       }
 
       swipeMagneticCard.here
+      insertChip.here
     }
 
 
